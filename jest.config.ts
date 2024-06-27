80497ebaa407fd69bc1e16e50daab20c99a05643
Héctor Plinio Navarro Navarro <73659203+hectorplinio@users.noreply.github.com>
2024-06-27 15:58:59 +0200

    feat: initial commit

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+};
